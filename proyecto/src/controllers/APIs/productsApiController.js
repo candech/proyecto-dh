b497a51a4dbac2488f65e4f9380958a333f60fe1
candech <xknd21057@gmail.com>
2024-03-16 21:31:13 -0300

    cambios APIS: products

--- proyecto/src/controllers/APIs/productsApiController.js
+++ proyecto/src/controllers/APIs/productsApiController.js
@@ -5,32 +5,27 @@ const productsApiController = {
 
     products: async(req,res)=>{
         try {
-            let products = await db.Productos.findAll(
-                {include: ['category']}
-            )
-            //let categories = await db.Categoria.findAll()
-            return res.json({
-                meta:{
-                    status: 200,
-                    count: products.length,
-                    detail: req.originalUrl,
-                },
-                /* countByCategory:{
-                    categories:{
-                        inSale: inSale.length
-                    }
-                }, */
-                data: {...products},
-               })
-        } catch (error) {
-            console.log(error.message)
-        }
-    },
-   
-    detail: async(req,res)=>{
-        try {
-            let product = await db.Productos.findByPk(req.params.id)
-            const imageUrl = `${req.protocol}://${req.get('host')}/img/products/${product.image}`;
+            let products = await db.Productos.findAll()
+                    return res.json({
+                        meta:{
+                            status: 200,
+                            count: products.length,
+                            detail: req.originalUrl,
+
+                        },
+                        data: {...products},
+                    })
+                } catch (error) {
+                    console.log(error.message)
+                }
+            },
+            
+            detail: async(req,res)=>{
+                try {
+                    let product = await db.Productos.findByPk(req.params.id,
+                        {include: ['category']}
+                        )
+                        const imageUrl = `${req.protocol}://${req.get('host')}/img/products/${product.image}`;
             return res.status(200).json({
                 meta:{
                 status: 200,
